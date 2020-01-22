27a4237e6f0591bb56a5f9f6d0ddffeac372043b
daradona10 <daradona10@gmail.com>
2020-01-22 12:43:06 +0000

    allowed host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME')]
+ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 'daradona10-milestoneproject4.herokuapp.com']
 
 
 # Application definition
