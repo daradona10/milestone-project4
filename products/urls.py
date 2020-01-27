54428c55f42d349293fc295570fa77fc147b2824
daradona10 <daradona10@gmail.com>
2020-01-27 14:06:08 +0000

    updating products urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, all_jerseys, all_shorts, all_socks, all_base_layers, all_footballs
 
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^jerseys/$', all_jerseys, name='products'),
+    url(r'^shorts/$', all_shorts, name='products'),
+    url(r'^socks/$', all_socks, name='products'),
+    url(r'^base_layers/$', all_base_layers, name='products'),
+    url(r'^footballs/$', all_footballs, name='products'),
 ]
