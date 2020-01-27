8cfe99be6fae2d7d543ff160e47a1347cafa9f9a
daradona10 <daradona10@gmail.com>
2020-01-27 14:16:53 +0000

    updating products views file

--- products/views.py
+++ products/views.py
@@ -7,3 +7,23 @@ def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products": products})
 
+def all_jerseys(request):
+    products = Product.objects.filter(category='jersey')
+    return render(request, "products.html", {"products": products})
+
+def all_shorts(request):
+    products = Product.objects.filter(category='shorts')
+    return render(request, "products.html", {"products": products})
+
+def all_socks(request):
+    products = Product.objects.filter(category='socks')
+    return render(request, "products.html", {"products": products})
+
+def all_base_layers(request):
+    products = Product.objects.filter(category='base_layers')
+    return render(request, "products.html", {"products": products})
+
+def all_footballs(request):
+    products = Product.objects.filter(category='football')
+    return render(request, "products.html", {"products": products})
+
